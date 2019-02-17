b47a9cb75f70b7ea238e7f6277c61ca29ffbe5b8
欧阳宇瀚 <479305102@qq.com>
2019-02-17 20:58:17 +0800

    feat: 完成jwt可配置无需验证用户的接口

--- utils/decodeToken.js
+++ utils/decodeToken.js
@@ -1,19 +1,24 @@
 const jwt = require("jsonwebtoken");
 const response = require("../utils/response");
+const unless = require("express-unless");
 
-module.exports = function(req, res, next) {
-  const token =
-    req.body.token || req.query.token || req.headers["authorization"];
-  if (token) {
-    jwt.verify(token, 'orange', function(err, decoded) {
-      if (err) {
-        return res.send(response("token信息错误", "token_wrong"));
-      } else {
-        req.userInfo = decoded;
-        next();
-      }
-    })
-  } else {
-    return res.status(403).send(response("未找到token", "token_not_found"))
-  }
-}
+module.exports = function(options) {
+  let decodeToken = function(req, res, next) {
+    const token =
+      req.body.token || req.query.token || req.headers["authorization"];
+    if (token) {
+      jwt.verify(token, "orange", function(err, decoded) {
+        if (err) {
+          return res.send(response("token信息错误", "token_wrong"));
+        } else {
+          req.userInfo = decoded;
+          next();
+        }
+      });
+    } else {
+      return res.status(403).send(response("未找到token", "token_not_found"));
+    }
+  };
+  decodeToken.unless = unless;
+  return decodeToken;
+};
